5fe5343238c5c0ab0c2ad7fc79b97452869959e3
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 22:12:49 +0100

    added HttpClient

--- src/XamarinApplication.Core.DependencyInjection/WeatherClientExtensions.cs
+++ src/XamarinApplication.Core.DependencyInjection/WeatherClientExtensions.cs
@@ -1,6 +1,19 @@
-﻿namespace XamarinApplication.Core.DependencyInjection
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace XamarinApplication.Core.DependencyInjection
 {
-    public class WeatherClientExtensions
+    public static class WeatherClientExtensions
     {
+        public static IHttpClientBuilder AddWeatherClient(this IServiceCollection services)
+        {
+            var httpClientBuilder = services.AddHttpClient<IWeatherClient, WeatherClient>(httpClient =>
+            {
+                httpClient.BaseAddress = new Uri($"{Constants.BaseUrl}{Constants.ApiKey}");
+                return new WeatherClient(httpClient);
+            });
+
+            return httpClientBuilder;
+        }
     }
 }
